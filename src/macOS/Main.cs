3deeae6984beaa2626cee8a4c91b599d7afa2854
Robert Sundström <robert.sundstrom@outlook.com>
2018-02-17 15:59:38 +0100

    Add macOS project with Xamarin.Forms support

---
+++ src/macOS/Main.cs
@@ -0,0 +1,15 @@
+﻿using AppKit;
+
+namespace Axis.AudioPlayer.macOS
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.SharedApplication.Delegate = new AppDelegate();
+            NSApplication.SharedApplication.MainMenu = new NSMenu();
+            NSApplication.Main(args);
+        }
+    }
+}
