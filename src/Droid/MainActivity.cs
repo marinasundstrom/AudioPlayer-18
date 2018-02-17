7b933b0433a80b7d940c51e72dc95637eddf06da
Robert Sundström <robert.sundstrom@outlook.com>
2018-02-17 12:35:00 +0100

    Update app names, packages, and set C# version to latest.

--- src/Droid/MainActivity.cs
+++ src/Droid/MainActivity.cs
@@ -10,7 +10,7 @@ using Android.OS;
 
 namespace Axis.AudioPlayer.Droid
 {
-    [Activity(Label = "Axis.AudioPlayer.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Audio Player", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
