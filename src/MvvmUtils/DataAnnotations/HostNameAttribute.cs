de431bae721b0db07429bbe82baa0e709fa7efc1
Robert Sundström <robert.sundstrom@outlook.com>
2018-02-18 10:40:42 +0100

    Add DataAnnotations for validation to MvvmUtils

---
+++ src/MvvmUtils/DataAnnotations/HostNameAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvvmUtils.DataAnnotations
+{
+    public class HostNameAttribute : ValidationAttribute
+    {
+        public HostNameAttribute(string errorMessage) : base(errorMessage)
+        {
+        }
+
+        public override bool IsValid(object value)
+        {
+            try
+            {
+                return Uri.CheckHostName(value as string) != UriHostNameType.Unknown;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ src/MvvmUtils/DataAnnotations/IPAddressAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace MvvmUtils.DataAnnotations
+{
+    public class IPAddressAttribute : ValidationAttribute
+    {
+        public IPAddressAttribute(string errorMessage) : base(errorMessage)
+        {
+        }
+
+        public override bool IsValid(object value)
+        {
+            try 
+            {
+                IPAddress.Parse(value as string);
+                return true;
+            } 
+            catch (Exception) 
+            {
+                return false;
+            }
+        }
+    }
+}
