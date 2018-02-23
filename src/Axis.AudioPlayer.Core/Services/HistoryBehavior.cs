5d8c69123f020b9084785fe5b9b582f46aae0b65
Robert Sundström <robert.sundstrom@outlook.com>
2018-02-23 23:32:19 +0100

    Add NavigationService

---
+++ src/Axis.AudioPlayer.Core/Services/HistoryBehavior.cs
@@ -0,0 +1,8 @@
+﻿namespace Axis.AudioPlayer.Services
+{
+    public enum HistoryBehavior
+    {
+        Default,
+        ClearHistory
+    }
+}
---
+++ src/Axis.AudioPlayer/Services/NavigationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace Axis.AudioPlayer.Services
+{
+    public static class NavigationExtensions
+    {
+        private static readonly ConditionalWeakTable<Page, object> arguments
+            = new ConditionalWeakTable<Page, object>();
+
+        public static object GetNavigationArgs(this Page page)
+        {
+            arguments.TryGetValue(page, out var argument);
+            return argument;
+        }
+
+        public static void SetNavigationArgs(this Page page, object args)
+            => arguments.Add(page, args);
+    }
+}
